769196538fb781c7cb35d56b9b0181ef1690764f
Michael Himing <mhiming@gmail.com>
2019-04-07 21:43:02 +1000

    argv

--- cgio/_testcase.py
+++ cgio/_testcase.py
@@ -9,9 +9,9 @@ class TestCase(unittest.TestCase):
     maxDiff = None
 
     @contextmanager
-    def assertOutput(self, expected, *, argv=["-"], strip_trailing=True):
+    def assertOutput(self, expected, *, argv=[], strip_trailing=True):
         fake_stdout = StringIO()
-        with patch.object(sys, "argv", argv):
+        with patch.object(sys, "argv", ["-"] + argv):
             with patch.object(sys, "stdout", fake_stdout):
                 yield fake_stdout
         output = fake_stdout.getvalue()
